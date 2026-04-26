f6564e6bfe5f0a813805ad456890aa096f19808d
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2026-04-26 20:39:33 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dp(n):
+            if n in memo:
+                return memo[n]
+
+            if n >= len(nums):
+                return 0
+
+            memo[n] = max(nums[n] + dp(n + 2), dp(n + 1))
+            return memo[n]
+
+        return dp(0)
